694de2a4313c164f7f65c56844f9f896a912bf8a
kogama-history <kogama-history@openkogama.invalid>
2023-12-19 09:22:38 +0000

    2.30.68 (2022.3.13f1)
    Build-Id: f8cb0ade-b30c-4939-a26e-5bfdbf414cb4
    Build-Sha256: a5b3c01ee9fda97c6f4517bec63f33af11ee1810a69a0dfb672fad263b4d8add

--- cs/MVWorldObject/_PrivateImplementationDetails_{F32C32B3-B1A0-47FA-AEB8-CD50B00CBCAD}.cs
+++ cs/MVWorldObject/_PrivateImplementationDetails_{F32C32B3-B1A0-47FA-AEB8-CD50B00CBCAD}.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 // Image 13: MVWorldObject.dll - Assembly: MVWorldObject, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 [CompilerGenerated]
-internal class _PrivateImplementationDetails__0674A13D_A5AE_4D85_88AF_4B354BDDADF5_
+internal class _PrivateImplementationDetails__F32C32B3_B1A0_47FA_AEB8_CD50B00CBCAD_
 {
 	// Fields
 	internal static long __method0x60003ed_1;
