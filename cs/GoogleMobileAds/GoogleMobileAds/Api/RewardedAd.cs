5c12524ecfd524a108bd2c5b14312a613db8838a
kogama-history <kogama-history@openkogama.invalid>
2023-12-21 10:25:53 +0000

    2.30.69 (2022.3.13f1)
    Build-Id: 11227806-7f6f-4e4e-9886-2a90121118ee
    Build-Sha256: 26f0ef4d2360296004d3b3b1fec8c330e169b0ce02968ac6fed60a5308c75947

--- cs/GoogleMobileAds/GoogleMobileAds/Api/RewardedAd.cs
+++ cs/GoogleMobileAds/GoogleMobileAds/Api/RewardedAd.cs
@@ -9,21 +9,6 @@ using System.Runtime.CompilerServices;
 using GoogleMobileAds.Common;
 
 // Image 38: GoogleMobileAds.dll - Assembly: GoogleMobileAds, Version=8.1.0.0, Culture=neutral, PublicKeyToken=null
-[assembly: AssemblyCompany("Google LLC")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright 2023 Google LLC. All Rights Reserved.")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyFileVersion("8.1.0")]
-[assembly: AssemblyProduct("GoogleMobileAds")]
-[assembly: AssemblyTitle("GoogleMobileAds")]
-[assembly: AssemblyTrademark("")]
-[assembly: Debuggable(DebuggableAttribute.DebuggingModes.None | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
-[assembly: InternalsVisibleTo("GoogleMobileAdsNative.unobfuscate")]
-[assembly: InternalsVisibleTo("GoogleMobileAdsNative")]
-[assembly: InternalsVisibleTo("GoogleMobileAds.Placement")]
-[assembly: InternalsVisibleTo("basic_unittests_test_library")]
-[assembly: InternalsVisibleTo("UnitTests")]
-[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 
 namespace GoogleMobileAds.Api
 {
--- cs/MVWorldObject/_PrivateImplementationDetails_{C7F29DC9-DF6C-42D7-AB81-BFB26D0EC83B}.cs
+++ cs/MVWorldObject/_PrivateImplementationDetails_{C7F29DC9-DF6C-42D7-AB81-BFB26D0EC83B}.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 // Image 13: MVWorldObject.dll - Assembly: MVWorldObject, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 [CompilerGenerated]
-internal class _PrivateImplementationDetails__F4F8CF12_58C1_4AFA_95FC_70377BE15B12_
+internal class _PrivateImplementationDetails__C7F29DC9_DF6C_42D7_AB81_BFB26D0EC83B_
 {
 	// Fields
 	internal static long __method0x60003ed_1;
