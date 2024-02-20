006a843726e4a1ff446e383a6020b8931e5f8172
kogama-history <kogama-history@openkogama.invalid>
2024-02-20 09:33:33 +0000

    2.30.59 (2022.3.13f1)
    Build-Id: 230e9a96-6167-4d53-8eee-57ed089a64d5
    Build-Sha256: 27387de652e1d2502275a0b42be19bded1702a1bc1303c7c3ab84fb52a8b4a3b

--- cs/Assembly-CSharp-firstpass/CodeStage/AntiCheat/Utils/xxHash.cs
+++ cs/Assembly-CSharp-firstpass/CodeStage/AntiCheat/Utils/xxHash.cs
@@ -7,6 +7,9 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 // Image 15: Assembly-CSharp-firstpass.dll - Assembly: Assembly-CSharp-firstpass, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+[assembly: CompilationRelaxations(8)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.None | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 
 namespace CodeStage.AntiCheat.Utils
 {
--- cs/MVWorldObject/_PrivateImplementationDetails_{B732F72A-DCA5-4B48-901A-858A5EC27229}.cs
+++ cs/MVWorldObject/_PrivateImplementationDetails_{B732F72A-DCA5-4B48-901A-858A5EC27229}.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 // Image 13: MVWorldObject.dll - Assembly: MVWorldObject, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 [CompilerGenerated]
-internal class _PrivateImplementationDetails__662B721F_BAB8_44B9_94BA_1BA72F3DA20B_
+internal class _PrivateImplementationDetails__B732F72A_DCA5_4B48_901A_858A5EC27229_
 {
 	// Fields
 	internal static long __method0x60003fc_1;
