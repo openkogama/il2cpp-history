7637be79a77ffc9f9d138b9844de2312f23a7a5c
kogama-history <kogama-history@openkogama.invalid>
2024-05-16 15:01:48 +0000

    2.30.80 (2022.3.13f1)
    Build-Id: 506f874e-2b45-454f-9216-b3f0ff1eaf31
    Build-Sha256: d345744467e3e9d0785a9c12b3c254aeea1ceedebf9acd3aa70a095e67df634c

--- cs/Assembly-CSharp-firstpass/SpecularLighting.cs
+++ cs/Assembly-CSharp-firstpass/SpecularLighting.cs
@@ -8,6 +8,9 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 
 // Image 15: Assembly-CSharp-firstpass.dll - Assembly: Assembly-CSharp-firstpass, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+[assembly: CompilationRelaxations(8)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.None | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 
 [ExecuteInEditMode]
 [RequireComponent(typeof(WaterBase))]
