cdebe00544d28160587ef11280a9ff3834ba3197
kogama-history <kogama-history@openkogama.invalid>
2024-10-07 11:39:03 +0000

    3.1.7 (2022.3.13f1)
    Build-Id: e496e62c-55ab-4bd9-963b-c159f5db4afd
    Build-Sha256: 9e03faa6f757b5f15c649c253e8a69818b14b3971358008b9affec768b986858

--- cs/Photon3Unity3D/_PrivateImplementationDetails_.cs
+++ cs/Photon3Unity3D/_PrivateImplementationDetails_.cs
@@ -9,6 +9,17 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Image 12: Photon3Unity3D.dll - Assembly: Photon3Unity3D, Version=4.1.2.19, Culture=neutral, PublicKeyToken=null
+[assembly: AssemblyCompany("Exit Games GmbH")]
+[assembly: AssemblyConfiguration(".NET Framework 3.5")]
+[assembly: AssemblyCopyright("(c) Exit Games GmbH, http://www.exitgames.com")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Debug. Unity Mono compatible")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(false)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.None | DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: Guid("177c1da7-4b4e-4209-8d90-9d8830e6e950")]
+[assembly: InternalsVisibleTo("NUnit")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 
 [CompilerGenerated]
 internal sealed class _PrivateImplementationDetails_
