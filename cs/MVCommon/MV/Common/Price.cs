b72a60f6e0d7d8ed900d9e02393bc792112ab992
kogama-history <kogama-history@openkogama.invalid>
2025-04-07 08:51:50 +0000

    3.1.44 (2022.3.43f1)
    Build-Id: 4a2bf3b1-1220-4400-a55b-2481e9dde2d7
    Build-Sha256: fd57def419431e6465061154b28485febd8828ea73911262e433d25b906c50b2

--- cs/MVCommon/MV/Common/Price.cs
+++ cs/MVCommon/MV/Common/Price.cs
@@ -10,20 +10,6 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
 // Image 25: MVCommon.dll - Assembly: MVCommon, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright \u00A9 Microsoft 2011")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyProduct("MVCommon")]
-[assembly: AssemblyTitle("MVCommon")]
-[assembly: AssemblyTrademark("")]
-[assembly: CompilationRelaxations(8)]
-[assembly: ComVisible(false)]
-[assembly: Debuggable(DebuggableAttribute.DebuggingModes.None | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
-[assembly: Guid("0e585335-9dd1-49f8-9c43-0cd3515877fe")]
-[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
-[assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
 
 namespace MV.Common
 {
